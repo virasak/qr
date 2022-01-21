eb2dc8c501febfc337918f1514883ef4279aa9e6
Virasak Dungsrikaew <virasak@gmail.com>
2022-01-21 09:41:08 +0700

    deploy to gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : '/qr'
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
